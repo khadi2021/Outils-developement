780ad1c07ad32d395b711ab1f63538d9015ae5aa
DESKTOP-L6JMRVS\Hello <mint2nekhadi@gmail.com>
2021-12-16 15:13:03 +0000

    TP1

---
+++ src/main/java/mr/iscae/apimodules/ApiModulesApplication.java
@@ -0,0 +1,13 @@
+package mr.iscae.apimodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiModulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mr/iscae/apimodules/ApiModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.iscae.apimodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
